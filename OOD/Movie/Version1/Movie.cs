80e30816ca9e994467c9ac54a1e44d7a116b3d9c
Azam Azam <mdazam@microsoft.com>
2021-01-14 12:17:36 -0800

    added Object

---
+++ OOD/Movie/Version1/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie.Version1
+{
+    class Movie
+    {
+        public static readonly int CHILDRENS = 2;
+        public static readonly int REGULAR = 0; 
+        public static readonly int NEW_RELEASE = 1;
+
+        private string _title;
+        private int _priceCode;
+
+        public Movie(string title, int priceCode)
+        { 
+            _title = title;
+            _priceCode = priceCode;
+        }
+
+        public int getPriceCode() { return _priceCode; }
+        public void setPriceCode(int arg) { _priceCode = arg; }
+
+        public String getTitle() { return _title; }
+
+    }
+}
---
+++ OOD/Movie/Version1/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie.Version1
+{
+    class Rental
+    {
+        private Movie _movie;
+        private int _daysRented;
+        public Rental(Movie movie, int daysRented) { _movie = movie; _daysRented = daysRented; }
+        public int getDaysRented() { return _daysRented; }
+        public Movie getMovie() { return _movie; }
+    }
+}
